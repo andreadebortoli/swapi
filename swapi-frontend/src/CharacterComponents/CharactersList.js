76b39a51ed7012386f0f88a5e7837a3a889104e5
andreadebortoli <andreadebortoli@me.com>
2022-08-15 10:15:01 +0200

    Project setup

---
+++ swapi-frontend/src/CharacterComponents/CharactersList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import './CharactersList.css';
+
+
+
+export default function CharactersList ({characters}){
+    return (
+      <>
+      <div>
+      </div>
+      </>
+    )
+  
+}
